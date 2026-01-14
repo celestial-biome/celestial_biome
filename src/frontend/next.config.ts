83b46745ebcdd5ab3d34a87ad94d170e1e988325
Celestial Biome <celestialbiome@gmail.com>
2026-01-14 11:25:24 +0900

    fix

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -8,27 +8,39 @@ const nextConfig: NextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // Sentry Webpack Plugin Options
-  org: 'celestial-biome',
-  project: 'celestial-biome-frontend', // Terraform で作成した正しいプロジェクトSlugを指定
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  // ビルド中のソースマップアップロード時にログを表示しない（CIのログが見づらくなるため）
+  org: 'celestial-biome',
+
+  project: 'celestial-biome-frontend',
+
+  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // クライアント側のソースマップを広くアップロードする
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // React コンポーネント名を注釈として付ける（デバッグしやすくなる）
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
-  // 広告ブロッカー対策 (Sentryへの通信を自社ドメイン経由に見せる)
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
   tunnelRoute: '/monitoring',
 
-  // 開発サーバーの起動時間を短縮するためにロガーを無効化
-  disableLogger: true,
+  webpack: {
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
 
-  // Vercel Cron Monitoring の自動設定 (Cloud Run なので不要ですがあっても無害)
-  automaticVercelMonitors: true,
+    // Tree-shaking options for reducing bundle size
+    treeshake: {
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      removeDebugLogging: true,
+    },
+  },
 });
